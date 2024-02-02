905df0d7b7648323e64b3ffef25b5fe72a835b8d
Hero <68159082+HeroEyad@users.noreply.github.com>
2024-02-02 20:32:00 +0200

    musical number!
    zzzz

--- java/index.js
+++ java/index.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var audioButton = document.getElementById("me")
+  var audioPlay = document.getElementById("audio")
   var buttonContainer = document.getElementById("buttonContainer");
 
   var buttons = [
       { text: "Socials", url: "socials.html" }
   ];
+
+  audioButton.addEventListener("click", function() {
+    if(audioPlay.paused) {
+        audioPlay.play();
+    } else {
+        audioPlay.pause();
+    }
+  })
 
   buttons.forEach(function(buttonInfo) {
       var button = document.createElement("button");
