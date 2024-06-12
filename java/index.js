303965818fa608f2864193c020d1eaf9c44527af
Hero <68159082+HeroEyad@users.noreply.github.com>
2024-06-12 12:18:41 +0300

    Bootstrap 5, some new changes and updates

--- java/index.js
+++ java/index.js
@@ -1,56 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var audioButton = document.getElementById("me")
-  var audioPlay = document.getElementById("audio")
+  var audioPlay = document.getElementById("audio");
+  var audioButton = document.getElementById("me");
   var buttonContainer = document.getElementById("buttonContainer");
+  var overlay = document.getElementById("overlay");
+  var startButton = document.getElementById("startButton");
+  var loadingOverlay = document.getElementById("loadingOverlay");
 
   var buttons = [
       { text: "Socials", url: "socials.html" },
-      { text: "Examples", url: "https://twitter.com/HeroEyad_/status/1704444656140849251?t=-EeseU-yP3LBYEZJOXkJdA&s=19 "},
+      { text: "Examples", url: "https://twitter.com/HeroEyad_/status/1704444656140849251?t=-EeseU-yP3LBYEZJOXkJdA&s=19"},
       { text: "My OC", url: "oc.html"}
   ];
 
-  function createKofiWidget() {
-    var kofiScript = document.createElement("script");
-    kofiScript.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
-    kofiScript.async = true;
+  setTimeout(function() {
+    loadingOverlay.style.display = "none";
+    overlay.style.display = "flex";
+}, 5000);
 
-    kofiScript.onload = function() {
-      kofiWidgetOverlay.draw('heroeyad', {
-        'type': 'floating-chat',
-        'floating-chat.donateButton.text': 'Support me',
-        'floating-chat.donateButton.background-color': '#323842',
-        'floating-chat.donateButton.text-color': '#fff'
+  startButton.addEventListener("click", function() {
+      overlay.style.display = "none";
+      audioPlay.play().catch(function(error) {
+          console.log("Autoplay was prevented. User interaction is required.");
       });
-    };
-
-    return kofiScript;
-  }
+  });
 
   audioButton.addEventListener("click", function() {
-    if(audioPlay.paused) {
-        audioPlay.play();
-    } else {
-        audioPlay.pause();
-    }
-  })
+      if(audioPlay.paused) {
+          audioPlay.play();
+      } else {
+          audioPlay.pause();
+      }
+  });
 
   buttons.forEach(function(buttonInfo) {
       var button = document.createElement("button");
-      
       button.textContent = buttonInfo.text;
-
       button.classList.add("custom-button");
-      button.style.backgroundColor = buttonInfo.color;
 
       button.addEventListener("click", function() {
           window.location.href = buttonInfo.url;
       });
 
       buttonContainer.appendChild(button);
-      
-      button.style.margin = "10px";
   });
 
-  var kofiButton = createKofiWidget();
-  buttonContainer.appendChild(kofiButton);
+  var kofiScript = document.createElement("script");
+  kofiScript.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+  kofiScript.async = true;
+
+  kofiScript.onload = function() {
+      kofiWidgetOverlay.draw('heroeyad', {
+          'type': 'floating-chat',
+          'floating-chat.donateButton.text': 'Support me',
+          'floating-chat.donateButton.background-color': '#323842',
+          'floating-chat.donateButton.text-color': '#fff'
+      });
+  };
+
+  buttonContainer.appendChild(kofiScript);
 });
--- java/socials.js
+++ java/socials.js
@@ -1,36 +1,50 @@
-var buttonContainer2 = document.getElementById("buttonContainer2");
+document.addEventListener("DOMContentLoaded", function() {
+    var buttonContainer2 = document.getElementById("buttonContainer2");
 
-var buttons = [
-    { text: "Home", url: "index.html" }
-];
+    var buttons = [
+        { text: "Home", url: "index.html" },
+        { text: "My OC", url: "oc.html"},
+        { text: "Examples", url: "https://twitter.com/HeroEyad_/status/1704444656140849251?t=-EeseU-yP3LBYEZJOXkJdA&s=19"}
+    ];
 
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var button3 = document.getElementById("button3");
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3");
+    var button4 = doucment.getElementById("button4")
+    var button5 = document.getElementById("button5")
 
-button1.addEventListener("click", function() {
-    window.location.href = "https://twitter.com/HeroEyad_"
-});
-
-button2.addEventListener("click", function() {
-    window.location.href = "https://github.com/HeroEyad"
-})
-
-button3.addEventListener("click", function() {
-    window.location.href = "https://www.youtube.com/c/HeroEyad"
-})
-
-buttons.forEach(function(buttonInfo) {
-    var button = document.createElement("button");
-    
-    button.textContent = buttonInfo.text;
-
-    button.classList.add("custom-button");
-    button.style.backgroundColor = buttonInfo.color;
-
-    button.addEventListener("click", function() {
-        window.location.href = buttonInfo.url;
+    button1.addEventListener("click", function() {
+        window.location.href = "https://twitter.com/HeroEyad_";
     });
 
-    buttonContainer2.appendChild(button);
+    button2.addEventListener("click", function() {
+        window.location.href = "https://github.com/HeroEyad";
+    });
+
+    button3.addEventListener("click", function() {
+        window.location.href = "https://www.youtube.com/c/HeroEyad";
+    });
+
+    button4.addEventListener("click", function() {
+        window.location.href = "https://gamebanana.com/members/1826250"
+    });
+
+    button5.addEventListener("click", function() {
+        window.location.href = "https://steamcommunity.com/id/heroeyad8/"
+    });
+
+    buttons.forEach(function(buttonInfo) {
+        var button = document.createElement("button");
+        
+        button.textContent = buttonInfo.text;
+
+        button.classList.add("custom-button");
+        button.style.backgroundColor = buttonInfo.color;
+
+        button.addEventListener("click", function() {
+            window.location.href = buttonInfo.url;
+        });
+
+        buttonContainer2.appendChild(button);
+    });
 });
